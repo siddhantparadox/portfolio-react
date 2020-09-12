e98f17df6ee76b6e7860e3da67c5a747a4f2ab77
siddhantparadox <siddhantg314@gmail.com>
2020-09-13 00:18:26 +0530

    Updates

---
+++ precache-manifest.d79996744e7aa71ff0b4f71acb2e9296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6476439219659530ce9b1034c7e43745",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "97f5d9fd87f36ab3ff53",
+    "url": "/portfolio-react/static/css/main.6b16ff36.chunk.css"
+  },
+  {
+    "revision": "0a963805115bffa66761",
+    "url": "/portfolio-react/static/js/2.1d934201.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio-react/static/js/2.1d934201.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97f5d9fd87f36ab3ff53",
+    "url": "/portfolio-react/static/js/main.d08c50e6.chunk.js"
+  },
+  {
+    "revision": "feb6fec8f95565f3aa5d",
+    "url": "/portfolio-react/static/js/runtime-main.c94dc782.js"
+  },
+  {
+    "revision": "b30d7ed5e889e6c8b881d51a206efa38",
+    "url": "/portfolio-react/static/media/siddhantgupta.b30d7ed5.jpg"
+  }
+]);
